abc9d87d301a34267952301d7dc91f78b10a85e9
jamesigibbs <jamesigibbs180@gmail.com>
2020-11-16 17:28:27 +0000

    Added python file

---
+++ mysql-from-python.py
@@ -0,0 +1,22 @@
+import os
+import pymysql
+
+# Set username
+username = 'root'
+
+# Connect database
+connection = pymysql.connect(host='localhost',
+                             user=username,
+                             password='',
+                             db='Chinook')
+
+try:
+    # Run a query
+    with connection.cursor() as cursor:
+        sql = "SELECT * FROM Artist;"
+        cursor.execute(sql)
+        result = cursor.fetchall()
+        print(result)
+finally:
+    # Close the connection
+    connection.close()
